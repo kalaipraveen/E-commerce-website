3ac22240aec11b74c017c576f542428e6e00f0dc
kalaipraveen <79854541+kalaipraveen@users.noreply.github.com>
2021-03-19 18:08:54 +0530

    updated Header Products Toolbar

--- src/Header/HeaderProductsToolbar.js
+++ src/Header/HeaderProductsToolbar.js
@@ -1,13 +1,6 @@
 import React from 'react'
 import './HeaderProductsToolbar.css'
-import Dropdown from 'react-dropdown'
-import 'react-dropdown/style.css'
-
-export default function HeaderProductsToolbar() {
-    const options = [
-        'one', 'two', 'three'
-      ];
-    const defaultOption = options[0];  
+ 
     return (
         <div className="header-products">
             Product categories
