368c339f1866693bec36d3c84d428e9652403809
kalaipraveen <79854541+kalaipraveen@users.noreply.github.com>
2021-03-19 18:12:53 +0530

    updated

--- src/Header/HeaderProductsToolbar.js
+++ src/Header/HeaderProductsToolbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './HeaderProductsToolbar.css'
- 
+
+export default function HeaderProductsToolbar() {
     return (
         <div className="header-products">
             Product categories
