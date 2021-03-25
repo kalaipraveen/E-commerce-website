0df51c03d42dcd3028db929045d29861142afb41
kalaipraveen <79854541+kalaipraveen@users.noreply.github.com>
2021-03-25 22:17:59 +0530

    refactored code

--- src/Body/Body.js
+++ src/Body/Body.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import './body.css'
-import CategoryInfo from './CategoryInfo'
-import CategoryTiles from './CategoryTiles'
-import TrendingNowTiles from './TrendingNowTiles'
+import CategoryInfoData from './CategoryInfoData.js'
+import categorytilesdata from './CategoryTilesData'
+import TrendingNowTilesdata from './TrendingNowTilesData'
 import BestsellerTiles from './BestSellerTiles'
 import CakesOnline from './CakesOnline'
 import PremiumGifts from './PremiumGifts'
@@ -36,4 +36,73 @@ export default function Body() {
     </div>
     </div>
   )
+}
+
+
+function CategoryInfo() {
+  return (
+      <div>
+          <div className="category-info">
+              {CategoryInfoData.map((categories, index) => (
+                  <div key={index}>
+                      <img src={categories.imagelink} alt="productimage" className="product-image" />
+                      <div className="product-name">{categories.name}</div>
+                  </div>
+              ))}
+          </div>
+          <div className="heading">
+              <h1 className="maintitle"><span className="highlight">Fresh Flowers</span> &amp; Perfect Gifts for all Occasions</h1>
+              <p className="mainsubtitle"><span className="highlight">3 Hour Delivery</span> &amp; Free Shipping in India. 66,732 Gift Ideas for your Beloved</p>
+          </div></div>
+
+  )
+}
+
+
+function CategoryTiles() {
+    return (
+        <div className="catagory-tiles">
+            <div class="category-title">Shop By Bestseller Categories
+                <a href="/gifts-bestsellers?promo=desk_hp_row1_pos_va" class="view-all">View All</a>
+            </div>
+            <div className="category-content">
+            {categorytilesdata.map((categorytiles, index) => (
+                <div key={index}>
+                <div className="category-card">
+                    <img src={categorytiles.imagelink} alt="categoriesimages" className="category-image" />
+                    <div className="catagorytiles-name">{categorytiles.name}</div>
+                </div>
+                </div>
+            ))}
+        </div>
+        </div>
+    )
+}
+
+
+
+
+
+function TrendingNowTiles() {
+    return (
+        <div>
+            <div className="trendingnow-tiles">
+                <div class="trendingnow-title">Trending Now
+                </div>
+                <div className="trendingnow-content">
+                    {TrendingNowTilesdata.map((trendingnowtiles, index) => {
+                        return (
+                            <div key={index}>
+                                <div className="trendingnow-card">
+                                    <img src={trendingnowtiles.imagelink} alt="trendingnowimages" className="trendingnow-image" />
+                                    <div className="trendingnowtiles-name">{trendingnowtiles.name}</div>
+                                    <div className="trendingnowtiles-description">{trendingnowtiles.description}</div>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+        </div>
+    )
 }
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import HeaderInfoToolbar from './HeaderInfoToolbar'
-import HeaderMainToolbar from './HeaderMainToolbar'
 import ImageSlider from './ImageSlider'
 import { SliderData } from './SliderData'
-import HeaderWrapper from './HeaderWrapper'
 import './Header.css'
+
+//Header info toolbar and Header wrapper
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+
+//Header main toolbar
+import fnplogo from '../Images/fnplogo.png'
+import cartlogo from '../Images/cartlogo.png'
+import userlogo from '../Images/userlogo.png'
+import qslogo from '../Images/qslogo.png'
+import timelogo from '../Images/timelogo.png'
+import SearchIcon from '@material-ui/icons/Search';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 
 export default function Header() {
     return (
@@ -14,5 +24,72 @@ export default function Header() {
             <HeaderWrapper />
             <ImageSlider slides={SliderData} />
         </div>
+    )
+}
+
+function HeaderInfoToolbar() {
+    return (
+        <div className="header-info">
+            <ul>
+                <li>Currency  INR <ArrowDropDownIcon /> </li>               
+                <li>Corporate Gifts</li>
+                <li>More <ArrowDropDownIcon /></li>
+            </ul>
+        </div>
+    )
+}
+
+function HeaderMainToolbar() {
+    return (
+        <div className="header-container">
+            <div className="logo-search-container">
+                <div className="logo-container">
+                    <img className="fnp-logo" src={fnplogo} alt="fnplogo" />
+                    <div className="delivery-container">
+                        <LocationOnIcon /> Select Delivery Location <KeyboardArrowRightIcon />
+                    </div>
+                    <form class="search-container">
+                        <input className="search-input" type="search" placeholder="search flowers, cakes, gifts etc" />
+                        <button id="searchbtn" data-ga-title="Search" class="searchFormButton">
+                            <SearchIcon fontSize="large" className="search-style" /> </button>
+                    </form>
+                </div>
+            </div>
+            <table className="header-cards">
+                <tr>
+                    <th className="header-cards-data"><img src={timelogo} alt="fnplogo" /></th>
+                    <th className="header-cards-data"><img src={cartlogo} alt="fnplogo" /></th>
+                    <th className="header-cards-data"><img src={userlogo} alt="fnplogo" /></th>
+                    <th className="header-cards-data"><img src={qslogo} alt="fnplogo" /></th>
+                </tr>
+                <tr>
+                    <td className="header-cards-data">Need Today?</td>
+                    <td className="header-cards-data">cart</td>
+                    <td className="header-cards-data">Hi Guest</td>
+                    <td className="header-cards-data">Hi Guest</td>
+                </tr>
+            </table>
+        </div>
+    )
+}
+
+function HeaderWrapper() {
+    return (
+            <div className="wrapper-info">
+                <ul>
+                    <li>Holi <ArrowDropDownIcon /></li>
+                    <li>Birthday <ArrowDropDownIcon /></li>
+                    <li>Anniversary <ArrowDropDownIcon /></li>
+                    <li>Cakes <ArrowDropDownIcon /></li>
+                    <li>Flowers <ArrowDropDownIcon /></li>
+                    <li>Personalized <ArrowDropDownIcon /></li>
+                    <li>Plants <ArrowDropDownIcon /></li>
+                    <li>Combos <ArrowDropDownIcon /></li>
+                    <li>Chocolates <ArrowDropDownIcon /></li>
+                    <li>More Gifts <ArrowDropDownIcon /></li>
+                    <li>Occasional <ArrowDropDownIcon /></li>
+                    <li>Global <ArrowDropDownIcon /></li>
+                </ul>
+            </div>
     )
 }
